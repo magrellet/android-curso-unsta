8c97d0e8041c6bfb7314cc4b23cb603c5e7c5933
mgrellet <martin.alejandro.grellet@gmail.com>
2017-10-18 20:34:21 -0300

    Clase 04 - Spinner(combo seleccionable) agregado en portrait latout

---
+++ holamundo - clase04/app/src/main/java/com/martin/holamundo/MainActivity.java
@@ -0,0 +1,115 @@
+package com.martin.holamundo;
+
+import android.opengl.EGLExt;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String DEFAULT_SELECT_VALUE="Seleccione...";
+
+    EditText name;
+    EditText lastName;
+    EditText email;
+    EditText phone;
+    TextView printName;
+    TextView printLastName;
+    TextView printEmail;
+    TextView printPhone;
+    TextView printGender;
+    String selectedGender;
+
+    private Spinner combo;
+    private static String[] gender = new String[]{DEFAULT_SELECT_VALUE,"Masculino","Femenino"};
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        name = (EditText) findViewById(R.id.name);
+        lastName = (EditText) findViewById(R.id.last_name);
+        email = (EditText) findViewById(R.id.email);
+        phone = (EditText) findViewById(R.id.phone);
+
+        printName = (TextView) findViewById(R.id.print_name);
+        printLastName = (TextView) findViewById(R.id.print_last_name);
+        printEmail = (TextView) findViewById(R.id.print_email);
+        printPhone = (TextView) findViewById(R.id.print_phone);
+        printGender = (TextView) findViewById(R.id.print_gender);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, gender);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        combo = (Spinner)findViewById(R.id.combo);
+        combo.setAdapter(adapter);
+
+        combo.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view,
+                                       int position, long id) {
+                Object item = adapterView.getItemAtPosition(position);
+
+                if(!(item.toString().contains("Seleccione"))){
+                    selectedGender=item.toString();
+                }
+                //printGender.setText(item.toString());
+
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+    }
+
+    public void imprimir(View view){
+
+        printName.setText(name.getText());
+        printLastName.setText(lastName.getText());
+        printEmail.setText(email.getText());
+        printPhone.setText(phone.getText());
+        printGender.setText(selectedGender);
+
+        /*combo.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view,
+                                       int position, long id) {
+                Object item = adapterView.getItemAtPosition(position);
+
+
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });*/
+
+    }
+
+    public void clear(View view){
+        name.setText("");
+        lastName.setText("");
+        email.setText("");
+        phone.setText("");
+        combo.setSelection(0);
+
+        printName.setText("");
+        printLastName.setText("");
+        printEmail.setText("");
+        printPhone.setText("");
+        printGender.setText("");
+
+    }
+}
+
